b22461bd9e09338fbca802898e23c9ad4d4bcde4
Brendan T White <brendan@thespia.com>
2025-07-31 20:29:36 +0930

    Now with CMAKE build variables being used for app name, version, etc
    We need the following set in Projects:
    HF_APP_DESCRIPTION
    HF_APP_DISPLAY_NAME
    HF_APP_NAME
    HF_APP_VERSION
    HF_ORG_DOMAIN
    HF_ORG_NAME

--- main.cpp
+++ main.cpp
@@ -20,10 +20,46 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    MainWindow w;
-    w.show();
+
+    // from https://github.com/AndreiCherniaev/CMake_variable_to_code/tree/main
+    #define CFG(x) #x
+    #define CONFIG(x) CFG(x)
+
+    // from CMAKE
+    qDebug() << "----------------------------------- ";
+    qDebug() << "CMAKE_PROJECT_NAME"        << CONFIG(CMAKE_PROJECT_NAME);
+    qDebug() << "CMAKE_PROJECT_DESCRIPTION" << CONFIG(CMAKE_PROJECT_DESCRIPTION);
+    qDebug() << "CMAKE_PROJECT_VERSION"     << CONFIG(CMAKE_PROJECT_VERSION);
+
+    // from HF vars in Projects
+    qDebug() << "----------------------------------- ";
+    qDebug() << "HF_ORG_NAME"   << CONFIG(HF_ORG_NAME);
+    qDebug() << "HF_ORG_DOMAIN" << CONFIG(HF_ORG_DOMAIN);
+    qDebug() << "----------------------------------- ";
+    qDebug() << "HF_APP_NAME"           << CONFIG(HF_APP_NAME);
+    qDebug() << "HF_APP_DISPLAY_NAME"   << CONFIG(HF_APP_DISPLAY_NAME);
+    qDebug() << "HF_APP_DESCRIPTION"    << CONFIG(HF_APP_DESCRIPTION);
+    qDebug() << "HF_APP_VERSION"        << CONFIG(HF_APP_VERSION);
+
+    // Set stuff in app
+    a.setOrganizationName(CONFIG(HF_ORG_NAME));
+    a.setOrganizationDomain(CONFIG(HF_ORG_DOMAIN));
+
+    a.setApplicationName(CONFIG(HF_APP_NAME));
+    a.setApplicationDisplayName(CONFIG(HF_APP_DISPLAY_NAME));
+    a.setApplicationVersion(CONFIG(HF_APP_VERSION));
+
+    // from app
+    qDebug() << "----------------------------------- ";
+    qDebug() << "a.organizationName()"      << a.organizationName();
+    qDebug() << "a.organizationDomain()"    << a.organizationDomain();
+    qDebug() << "----------------------------------- ";
+    qDebug() << "a.applicationName()"           << a.applicationName();
+    qDebug() << "a.applicationDisplayName()"    << a.applicationDisplayName();
+    qDebug() << "a.applicationVersion()"        << a.applicationVersion();
 
 
+    qDebug() << "----------------------------------- ";
 
     Fixture f1 = Fixture{1,1,"Fred"};
     Fixture f2 = Fixture{1,2,"George"};
@@ -43,5 +79,7 @@ int main(int argc, char *argv[])
 
 
 
+    MainWindow w;
+    w.show();
     return a.exec();
 }
