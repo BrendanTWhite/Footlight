0eeaf27c71629848bacd6fa1dfed00ae535b07ee
Brendan T White <brendan@thespia.com>
2025-08-14 21:11:32 +0930

    boilerplate

---
+++ models/fixturemodel.cpp
@@ -0,0 +1,5 @@
+#include "fixturemodel.h"
+
+FixtureModel::FixtureModel(QObject *parent)
+    : QObject{parent}
+{}
---
+++ models/fixturemodel.h
@@ -0,0 +1,13 @@
+#ifndef FIXTUREMODEL_H
+#define FIXTUREMODEL_H
+
+class FixtureModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FixtureModel(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // FIXTUREMODEL_H
